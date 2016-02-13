e2e2e07de90bad65fbec6f185e10ad960e5ee876
rosa <gojiradam@gmail.com>
2016-02-13 15:59:31 +0200

    Close connection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,8 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,37 +15,54 @@ import java.net.Socket;
 public class Controller {
 
 
-    @FXML public Label onlineOffline;
-    @FXML public TextArea serverChatArea;
-    @FXML public TextField serverChatField;
+    @FXML
+    public Label onlineOffline;
+    @FXML
+    public TextArea serverChatArea;
+    @FXML
+    public TextField serverChatField;
     private DataOutputStream sendToClient;
     private DataInputStream getFromClient;
     private ServerSocket serverState;
     private Socket serverConnectionStatus;
-    private int port =6789;
-    private int numberOfConnetions=100;
+    private int port = 6789;
+    private int numberOfConnetions = 100;
 
-    public void connectToClient(){
+    public void connectToClient() {
         try {
-            serverState = new ServerSocket(port,numberOfConnetions);
-           // waitingForConnection();
-            setSteams();
-        }catch (IOException ioexception){
-            serverChatArea.appendText("IOException - Server connection error.");
+            serverState = new ServerSocket(port, numberOfConnetions);
+            while (true) {
+                try {
+                    waitingForConnection();
+                    setSteams();
+                } catch (EOFException eofexception) {
+                    serverChatArea.appendText("IOException - Server connection error.");
+                    eofexception.printStackTrace();
+                } finally {
+                    closeConnetion();
+                }
+            }
+        } catch (IOException ioexception) {
             ioexception.printStackTrace();
         }
     }
-    private void waitingForConnection() throws IOException{
+
+    private void waitingForConnection() throws IOException {
         serverChatArea.appendText("Waiting for connection...");
         serverConnectionStatus = serverState.accept();
         serverChatArea.appendText("Connected.");
         onlineOffline.setText("Online");
     }
-    private void setSteams() throws IOException{
+
+    private void setSteams() throws IOException {
         sendToClient = new DataOutputStream(serverConnectionStatus.getOutputStream());
         sendToClient.flush();
         getFromClient = new DataInputStream(serverConnectionStatus.getInputStream());
         serverChatArea.appendText("Steams UP.");
+    }
+
+    private void closeConnetion() {
+
     }
 
 }
