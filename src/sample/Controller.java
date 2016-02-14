201969ddfe4abd00bf1451f4e5761f8976e04263
rosa <gojiradam@gmail.com>
2016-02-14 19:32:18 +0200

    Connection status

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -12,7 +13,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Controller {
+public class Controller implements Runnable {
 
 
     @FXML
@@ -23,46 +24,81 @@ public class Controller {
     public TextField serverChatField;
     private DataOutputStream sendToClient;
     private DataInputStream getFromClient;
-    private ServerSocket serverState;
-    private Socket serverConnectionStatus;
+    private ServerSocket serverSocketState;
+    private Socket serverSocketConnectionStatus;
     private int port = 6789;
     private int numberOfConnetions = 100;
+    private Thread iThread;
 
     public void connectToClient() {
-        try {
-            serverState = new ServerSocket(port, numberOfConnetions);
-            while (true) {
-                try {
-                    waitingForConnection();
-                    setSteams();
-                } catch (EOFException eofexception) {
-                    serverChatArea.appendText("IOException - Server connection error.");
-                    eofexception.printStackTrace();
-                } finally {
-                    closeConnetion();
+        Thread runAndConnectToClient = new Thread(() -> {
+            try {
+                serverSocketState = new ServerSocket(port, numberOfConnetions);
+
+                while (true) {
+                    try {
+                        waitingForConnection();
+                        setSteams();
+                    } catch (EOFException eofexception) {
+                        serverChatArea.appendText("IOException - Server connection error.");
+                    } finally {
+                        closeConnetion();
+                    }
                 }
+            } catch (IOException ioexception) {
+                ioexception.printStackTrace();
             }
-        } catch (IOException ioexception) {
-            ioexception.printStackTrace();
-        }
+        });
+        runAndConnectToClient.start();
+        getMessage();
+    }
+
+    private void checkConnection() {
+        Thread runCheckConnection = new Thread((() -> {
+            //while up and run
+        })
+        );
+        runCheckConnection.start();
     }
 
     private void waitingForConnection() throws IOException {
         serverChatArea.appendText("Waiting for connection...");
-        serverConnectionStatus = serverState.accept();
+        serverSocketConnectionStatus = serverSocketState.accept();
         serverChatArea.appendText("Connected.");
         onlineOffline.setText("Online");
     }
 
     private void setSteams() throws IOException {
-        sendToClient = new DataOutputStream(serverConnectionStatus.getOutputStream());
+        sendToClient = new DataOutputStream(serverSocketConnectionStatus.getOutputStream());
         sendToClient.flush();
-        getFromClient = new DataInputStream(serverConnectionStatus.getInputStream());
+        getFromClient = new DataInputStream(serverSocketConnectionStatus.getInputStream());
         serverChatArea.appendText("Steams UP.");
     }
 
     private void closeConnetion() {
-
+        serverChatArea.appendText("Closing connection . . .");
+        try {
+            sendToClient.close();
+            getFromClient.close();
+            serverSocketState.close();
+        } catch (IOException ioexception) {
+            ioexception.printStackTrace();
+        }
     }
 
+    private void getMessage() {
+        iThread = new Thread(this);
+        iThread.run();
+    }
+
+    public void run() {
+        do {
+            try {
+                String msg = getFromClient.readUTF(); // <--- freeze GUI
+                Platform.runLater(() -> serverChatArea.appendText(msg));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (true);
+    }
 }
