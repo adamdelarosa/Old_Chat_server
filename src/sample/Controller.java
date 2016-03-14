c3d981c4e7350599cab725f25d2da7ee50fb50de
ROSA <gojiradam@gmail.com>
2016-03-14 12:37:28 +0100

    Connection status

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
 import java.io.*;
 import java.net.BindException;
 import java.net.ServerSocket;
@@ -12,12 +13,18 @@ import java.net.Socket;
 
 public class Controller implements Runnable {
 
-    @FXML private Label connectToClientText;
-    @FXML private Label setSteamsText;
-    @FXML private Label getFromClientText;
-    @FXML private Label testConnection;
-    @FXML public TextArea serverChatArea ;
-    @FXML public TextField serverChatField;
+    @FXML
+    private Label connectToClientText;
+    @FXML
+    private Label setSteamsText;
+    @FXML
+    private Label getFromClientText;
+    @FXML
+    private Label testConnection;
+    @FXML
+    public TextArea serverChatArea;
+    @FXML
+    public TextField serverChatField;
     private DataOutputStream sendToClient;
     private DataInputStream getFromClient;
     private ServerSocket serverSocketState;
@@ -29,39 +36,39 @@ public class Controller implements Runnable {
     private boolean getFromClientSwitch;
 
     public Controller() {
-       //Platform.runLater(()->{
+        //Platform.runLater(()->{
         //   connectToClient();
-       //    connectionStatus();
-     //  });
+        //    connectionStatus();
+        //  });
     }
 
 
     public void connectToClient() {
 
-            Thread runAndConnectToClient = new Thread(() -> {
-                try {
-                    serverSocketState = new ServerSocket(port, numberOfConnetions);
-                    while (!getFromClientSwitch) {
-                        try {
-                            waitingForConnection();
-                            System.out.print("connectTo");
-                            setSteams();
-                            getMessage();
-                        } catch (EOFException eofexception) {
-                            serverChatArea.appendText("\nIOException - Server connection error.");
-                        } finally {
-                            closeConnection();
-                        }
+        Thread runAndConnectToClient = new Thread(() -> {
+            try {
+                serverSocketState = new ServerSocket(port, numberOfConnetions);
+                while (!getFromClientSwitch) {
+                    try {
+                        waitingForConnection();
+                        System.out.print("connectTo");
+                        setSteams();
+                        getMessage();
+                    } catch (EOFException eofexception) {
+                        serverChatArea.appendText("\nIOException - Server connection error.");
+                    } finally {
+                        closeConnection();
                     }
-                } catch (BindException bindexception) {
-                    serverChatArea.appendText("\n Already connected.");
-                } catch (IOException ioexception) {
-                    ioexception.printStackTrace();
                 }
-            });
-            runAndConnectToClient.start();
-            connectToClientText.setText("ONLINE");
-            connectToClientText.setTextFill(javafx.scene.paint.Color.web("#0076a3"));
+            } catch (BindException bindexception) {
+                serverChatArea.appendText("\n Already connected.");
+            } catch (IOException ioexception) {
+                ioexception.printStackTrace();
+            }
+        });
+        runAndConnectToClient.start();
+        connectToClientText.setText("ONLINE");
+        connectToClientText.setTextFill(javafx.scene.paint.Color.web("#0076a3"));
     }
 
     private void waitingForConnection() throws IOException {
@@ -81,42 +88,42 @@ public class Controller implements Runnable {
 
     }
 
-@FXML
+    @FXML
     public void closeConnection() {
-    //need to return tof to work.
-    if(1>2) {
-        Platform.runLater(() -> {
-            serverChatArea.appendText("\nClosing connection . . .");
-            try {
-                getFromClientSwitch = true;
-                sendToClient.close();
-                setSteamsText.setText("OFFLINE");
-                getFromClient.close();
-                getFromClientText.setText("OFFLINE");
-                serverSocketState.close();
-                connectToClientText.setText("OFFLINE");
-                System.out.println("closeConnection - DONE.");
-            } catch (IOException ioexception) {
-                ioexception.printStackTrace();
-            }
-        });
-    }else{
-        System.out.println("NO CONNECTION.");
+        //need to return tof to work.
+        if (1 > 2) {
+            Platform.runLater(() -> {
+                serverChatArea.appendText("\nClosing connection . . .");
+                try {
+                    getFromClientSwitch = true;
+                    sendToClient.close();
+                    setSteamsText.setText("OFFLINE");
+                    getFromClient.close();
+                    getFromClientText.setText("OFFLINE");
+                    serverSocketState.close();
+                    connectToClientText.setText("OFFLINE");
+                    System.out.println("closeConnection - DONE.");
+                } catch (IOException ioexception) {
+                    ioexception.printStackTrace();
+                }
+            });
+        } else {
+            System.out.println("NO CONNECTION.");
+        }
     }
-}
 
-    public void connectionStatus(){
+    public void connectionStatus() {
 
         runConnectionStatus = new Thread(() -> {
-            while (true){
-                Platform.runLater(()->{
-                    if(socketConnectionStatus.isConnected()){
+            while (true) {
+                Platform.runLater(() -> {
+                    if (socketConnectionStatus.isConnected()) {
                         try {
                             runConnectionStatus.sleep(1000);
-                                System.out.println("STATUS: Connected.");
+                            System.out.println("STATUS: Connected.");
                         } catch (InterruptedException e) {
                         }
-                    }else{
+                    } else {
                     }
                 });
             }
@@ -125,14 +132,14 @@ public class Controller implements Runnable {
     }
 
 
-    public void sendMessage(){
+    public void sendMessage() {
         String messageOut = serverChatField.getText();
         try {
             sendToClient.writeUTF(messageOut);
             sendToClient.flush();
             serverChatField.setText("");
             Platform.runLater(() -> serverChatArea.appendText("\n" + messageOut));
-            }catch (IOException e) {
+        } catch (IOException e) {
             serverChatArea.appendText("\nMessage was not sent.");
             e.printStackTrace();
         }
@@ -149,8 +156,9 @@ public class Controller implements Runnable {
                 String msg = getFromClient.readUTF();
                 Platform.runLater(() -> serverChatArea.appendText(msg + "\n"));
 
-            } catch (EOFException eofexception){
-            } catch (IOException e){e.printStackTrace();
+            } catch (EOFException eofexception) {
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             Platform.runLater(() -> {
                 getFromClientText.setText("ONLINE");
@@ -158,12 +166,12 @@ public class Controller implements Runnable {
             });
         } while (!getFromClientSwitch);
 
-            while (true){
-                    if(socketConnectionStatus.isConnected()){
-                            System.out.println("STATUS: Connected.");
-                    }else{
-                    }
+        while (true) {
+            if (socketConnectionStatus.isConnected()) {
+                System.out.println("STATUS: Connected.");
+            } else {
             }
+        }
 
     }
 
