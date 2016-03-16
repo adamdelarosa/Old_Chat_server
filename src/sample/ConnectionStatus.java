b9332d57665e503b8dd9193cac0616708380049e
ROSA <gojiradam@gmail.com>
2016-03-16 18:30:25 +0100

    Connection status

--- src/sample/ConnectionStatus.java
+++ src/sample/ConnectionStatus.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.util.Duration;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by ROSA on 3/16/16.
@@ -11,24 +15,31 @@ public class ConnectionStatus implements Runnable {
 
     private Thread iconnectionRunTester;
 
+    Timer timer = new Timer();
+
     public void test() {
         System.out.println("here");
         iconnectionRunTester = new Thread(this);
         iconnectionRunTester.start();
     }
 
-@Override
-    public void run() {
-        Platform.runLater(() -> {
-            do {
-                try {
-                    System.out.println("Thread id: " + iconnectionRunTester.getId());
-                    iconnectionRunTester.sleep(1000);
 
-                } catch (InterruptedException interruptedException) {
-                    interruptedException.printStackTrace();
-                }
-            } while (true);
-        });
-    }
+
+
+
+
+        @Override
+    public void run() {
+
+                do {
+                    try {
+                        System.out.println("Thread id: " + iconnectionRunTester.getId());
+                        iconnectionRunTester.sleep(1000);
+
+
+                    } catch (InterruptedException interruptedException) {
+                        interruptedException.printStackTrace();
+                    }
+                } while (true);
+        }
 }
