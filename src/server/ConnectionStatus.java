c4b7eb820c8d94e27c93bd36e349070b020106e1
ROSA <gojiradam@gmail.com>
2016-03-22 15:55:13 +0100

    Connection status

--- src/server/ConnectionStatus.java
+++ src/server/ConnectionStatus.java
@@ -18,20 +18,24 @@ public class ConnectionStatus implements Runnable {
         stopButton.connectionStatusStop.setDisable(false);
         iThread = new Thread(this);
         iThread.start();
+        onlineOfflineText.connectionStatusActive.setText("ONLINE");
+        onlineOfflineText.connectionStatusActive.setTextFill(javafx.scene.paint.Color.web("#00FF00"));
     }
 
     public void killConnecionStatusCheck() {
         shutdown = false;
         startButton.connectionStatusStart.setDisable(false);
         stopButton.connectionStatusStop.setDisable(true);
+        onlineOfflineText.connectionStatusActive.setText("OFFLINE");
+        onlineOfflineText.connectionStatusActive.setTextFill(javafx.scene.paint.Color.web("#ff0000"));
     }
 
     @Override
     public void run() {
+        onlineOfflineText.connectionStatusActive.setText("ONLINE");
         while (shutdown) {
             try {
-                //Active or not:
-                onlineOfflineText.connectionStatusActive.setText("ONLINE");
+                //Active or not
                 startButton.connectionStatusStart.setDisable(true);
                 System.out.println("Thread id: ");
                 iThread.sleep(1000);
